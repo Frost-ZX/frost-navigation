6667bb1c6792eb7e94e5d9f26981f38cdea4055e
Frost-ZX <30585462+Frost-ZX@users.noreply.github.com>
2021-05-03 00:54:43 +0800

    run-js 工具内容页（未完成）

---
+++ src/components/tools/OtherRunJS.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="detail-elem">
+        开发中，敬请期待。
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OtherRunJS'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
