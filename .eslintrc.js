70b78a18d5b054dbdac431e7e0aeb850bef32135
Frost-ZX <30585462+Frost-ZX@users.noreply.github.com>
2021-05-22 18:13:49 +0800

    分离配置文件

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/recommended',
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'vue/html-closing-bracket-newline': 'off',
+        'vue/html-indent': 'off',
+        'vue/html-self-closing': 'off',
+        'vue/max-attributes-per-line': 'off',
+        'vue/multiline-html-element-content-newline': 'off',
+        'vue/singleline-html-element-content-newline': 'off'
+    }
+};
