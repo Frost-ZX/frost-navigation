e53fb85bd30c4c859e60beaac4208e5d66f3bdac
Frost-ZX <30585462+Frost-ZX@users.noreply.github.com>
2021-06-14 20:47:26 +0800

    utils.js 移除 log()

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -7,30 +7,13 @@ class Utils {
     /**
      * 改变网页标题
      * 
-     * @param {string} value 新的标题
+     * @param {string} [value] 新的标题
      */
     changeTitle(value) {
         if (value) {
-            document.title = config.siteName + ' - '  + value;
+            document.title = config.siteName + ' - ' + value;
         } else {
             document.title = config.siteName;
-        }
-    }
-
-    /**
-     * 输出日志
-     * 
-     * @param {string} type 日志类型（info、warn、error）
-     * @param {string} title 日志标题
-     * @param {any} datas 日志内容
-     */
-    log(type, title, ...datas) {
-        if (type == 'info') {
-            console.info(`[${title}]`, datas);
-        } else if (type == 'warn') {
-            console.warn(`[${title}]`, datas);
-        } else if (type == 'error') {
-            console.error(`[${title}]`, datas);
         }
     }
 
