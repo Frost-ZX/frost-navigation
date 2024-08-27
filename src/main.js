6f39070e9b83c7e3fe35c4e47235aa5e212144d8
Frost-ZX <frost-zx@foxmail.com>
2024-08-27 18:44:46 +0800

    chore: 添加 Material Design Icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import { router } from './router';
 
+import '@mdi/font/css/materialdesignicons.css';
+
 import App from './App.vue';
 
 const app = createApp(App);
