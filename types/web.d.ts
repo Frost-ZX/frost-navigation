b76dc08395cf47b8bf749f138dd89f991d02276e
Frost-ZX <frost-zx@foxmail.com>
2024-08-31 21:26:23 +0800

    chore: 添加导航链接类型定义信息

--- types/web.d.ts
+++ types/web.d.ts
@@ -9,8 +9,32 @@ import type {
 
 declare global {
 
+  /** 导航链接项 */
+  interface NavLinkItem {
+    /** 链接分类标题 / 链接项标题 */
+    title: string;
+    /** 子链接列表 */
+    children?: NavLinkItem[];
+    /** 链接项更新日期 */
+    date?: string,
+    /** 链接项简介 */
+    desc?: string;
+    /** 链接分类图标 class */
+    icon?: string;
+    /** 链接是否已失效 */
+    isInvalid?: boolean;
+    /** 是否只显示详情，不跳转 */
+    showOnly?: boolean;
+    /** 链接项 URL */
+    url?: string;
+  };
+
   // window
   interface Window {
+
+    /** 导航链接列表 */
+    NAV_LINK_LIST: NavLinkItem[];
+
   }
 
   // Vue
