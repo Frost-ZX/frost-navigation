de004b4e0b3dfbc529d3a486ca2e2441d5dff403
Frost-ZX <frost-zx@foxmail.com>
2024-08-31 14:02:26 +0800

    chore: 引入 Lunisolar

--- src/main.js
+++ src/main.js
@@ -3,7 +3,27 @@ import { router } from './router';
 
 import '@mdi/font/css/materialdesignicons.css';
 
+import lunisolar from 'lunisolar';
+import lunisolarLang1 from 'lunisolar/locale/en';
+import lunisolarLang2 from 'lunisolar/locale/zh-cn';
+import lunisolarFestivals from 'lunisolar/markers/festivals.zh-cn';
+
 import App from './App.vue';
+
+// 加载 Lunisolar 语言包
+lunisolar.locale([
+  lunisolarLang1,
+  lunisolarLang2,
+], false);
+
+// 加载 Lunisolar 节日列表
+lunisolar.Markers.add(lunisolarFestivals, 'festivals');
+
+// Lunisolar 全局配置
+lunisolar.config({
+  isUTC: false,
+  lang: 'zh-cn',
+});
 
 const app = createApp(App);
 
