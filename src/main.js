10ab2fb670e7898907c709eb9cde8022dbeae049
Frost-ZX <frost-zx@foxmail.com>
2024-09-08 16:04:20 +0800

    chore(app): 引入 Day.js

--- src/main.js
+++ src/main.js
@@ -4,12 +4,24 @@ import { router } from './router';
 import '@mdi/font/css/materialdesignicons.css';
 import '@/assets/fonts/index.css';
 
+import dayjs from 'dayjs';
+import dayjsDuration from 'dayjs/plugin/duration';
+
+import 'dayjs/locale/zh-cn';
+import 'dayjs/locale/zh-hk';
+
 import lunisolar from 'lunisolar';
 import lunisolarLang1 from 'lunisolar/locale/en';
 import lunisolarLang2 from 'lunisolar/locale/zh-cn';
 import lunisolarFestivals from 'lunisolar/markers/festivals.zh-cn';
 
 import App from './App.vue';
+
+// 加载 Day.js Duration 插件
+dayjs.extend(dayjsDuration);
+
+// 配置 Day.js 默认语言
+dayjs.locale('zh-cn');
 
 // 加载 Lunisolar 语言包
 lunisolar.locale([
